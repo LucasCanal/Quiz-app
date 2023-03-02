a980247c7c0e58f8c7685f427b5e684798800d95
Lucas <c_nall@outlook.com>
2023-03-02 10:37:28 -0300

    first commit

---
+++ questions.js
@@ -0,0 +1,34 @@
+export default [
+    {
+      question: "Quanto é 30 + 30?",
+      answers: [
+        { option: "60", correct: true },
+        { option: "40", correct: false },
+        { option: "70", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 6 x 9?",
+      answers: [
+        { option: "50", correct: false },
+        { option: "45", correct: true },
+        { option: "35", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 10 x 6?",
+      answers: [
+        { option: "600", correct: false },
+        { option: "50", correct: false },
+        { option: "60", correct: true },
+      ],
+    },
+    {
+      question: "Quanto é 9 x 9?",
+      answers: [
+        { option: "63", correct: false },
+        { option: "79", correct: false },
+        { option: "81", correct: true },
+      ],
+    },
+  ];
